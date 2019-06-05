0f0cefe11ab8948ed0bb27a857f4528984ee4a15
huge <823865619@qq.com>
2019-06-05 14:08:03 +0800

    Initial commit

---
+++ src/com/bw/test/TestHW.java
@@ -0,0 +1,15 @@
+package com.bw.test;
+
+import java.util.ArrayList;
+
+/**
+ * Description:变量描述
+ * Author:胡正林823865618@qq.com)
+ * Date:2019/6/5-10:58-05
+ */
+public class TestHW {
+    public static void main(String[] args) {
+        ArrayList<Object> objects = new ArrayList<>();
+        String s = new String();
+    }
+}
---
+++ src/com/bw/test/TestHelloWorld.java
@@ -0,0 +1,21 @@
+package com.bw.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ */
+public class TestHelloWorld {
+    //入门程序
+    public static void main(String[] args) {
+        System.out.println("Hello,Ideal!");
+        method01();
+    }
+
+    private static void method01() {
+        ArrayList list = new ArrayList();
+        HashMap<Object, Object> map = new HashMap<>();
+
+    }
+}
